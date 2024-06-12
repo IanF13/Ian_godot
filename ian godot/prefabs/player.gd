40f186dd7c5e60aa39b808897b7886f9bfacbb14
IanF13 <130380548+IanF13@users.noreply.github.com>
2024-06-12 09:35:48 -0300

    Add files via upload

--- ian godot/prefabs/player.gd
+++ ian godot/prefabs/player.gd
@@ -1,16 +1,34 @@
 extends Area2D
 
 const SPEED := 400
+@onready var anim = $Anim
+@onready var collision_shape_2d = $CollisionShape2D
+@onready var screen_size = get_viewport_rect ().size
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var velocity  = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
-	if velocity.lenght() > 0:
+	if velocity.length() > 0:
 		velocity = velocity.normalized() * SPEED
 		
+	if velocity.x != 0 :
+		anim.play("move")
+	elif velocity.y > 0:
+		anim.play("move_up")
+	elif velocity.y < 0:
+		anim.play("move_down")
+	else: 
+		anim.play("idle")
+		
+	if velocity.x > 0:
+		anim.flip_h = false
+	else:
+		anim.flip_h = true 
+	
 	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
